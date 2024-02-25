2f5357b725011a52162fb5e18627953b389ae25c
ju-ki <juki.programming@gmail.com>
2024-02-25 21:42:35 +0900

    フロント実装+バックエンドの環境構築

---
+++ client/src/components/FormField.tsx
@@ -0,0 +1,55 @@
+type FormFieldProps = {
+  labelName:string;
+  type:string;
+  name:string;
+  placeholder:string;
+  value:string;
+  isSurpriseMe?:boolean;
+  handleChange?: () => void;
+  handleSurpriseMe?: () => void;
+}
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  isSurpriseMe,
+  handleChange,
+  handleSurpriseMe
+}:FormFieldProps) => {
+  return (
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-500"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] block w-full p-3"
+        />
+    </div>
+  )
+}
+
+export default FormField
